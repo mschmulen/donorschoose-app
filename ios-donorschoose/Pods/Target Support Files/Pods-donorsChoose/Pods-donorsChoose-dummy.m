e36ff19cf582afee9b68761cf6c6be758127e157
Matt Schmulen <matt.schmulen@gmail.com>
2018-03-30 09:21:49 -0500

    pods

---
+++ ios-donorschoose/Pods/Target Support Files/Pods-donorsChoose/Pods-donorsChoose-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_donorsChoose : NSObject
+@end
+@implementation PodsDummy_Pods_donorsChoose
+@end
---
+++ ios-donorschoose/Pods/Target Support Files/Pods-donorsChooseUITests/Pods-donorsChooseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_donorsChooseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_donorsChooseUITests
+@end
