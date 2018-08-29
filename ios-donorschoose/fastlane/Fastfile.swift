7877038123ef3dd8c8a770da3b00012b9266523b
mschmulen <matt.schmulen@gmail.com>
2018-08-29 13:39:13 -0500

    fastlane

--- ios-donorschoose/fastlane/Fastfile.swift
+++ ios-donorschoose/fastlane/Fastfile.swift
@@ -6,11 +6,57 @@
 //     https://docs.fastlane.tools/actions
 //
 
+/*
+fastlane [lane] key:value key2:value2
+
+fastlane deploy submit:false build_number:24
+
+
+func deployLane(withOptions options:[String: String]?) {
+ ...
+
+*/
 import Foundation
 
 class Fastfile: LaneFile {
-	func screenshotsLane() {
-	desc("Generate new localized screenshots")
-		captureScreenshots(workspace: "donorsChoose.xcworkspace", scheme: "Pods-donorsChooseUITests")
+
+	func testLane() {
+    	buildIosApp(scheme: "donorsChoose")
 	}
+
+	func helper() {
+    	// This is not a lane but can be called from a lane
+	}
+	
+	func screenshotsLane() {
+		desc("Generate new localized screenshots")
+		captureScreenshots(workspace: "donorsChoose.xcworkspace", scheme: "donorsChoose")
+	}
+	
+	/*
+	func betaLane() {
+	    desc("Submit a new Beta Build to Apple TestFlight. This will also make sure the profile is up to date")
+
+	    syncCodeSigning(gitUrl: "URL/for/your/git/repo", appIdentifier: [appIdentifier], username: appleID)
+	    // Build your app - more options available
+	    buildIosApp(scheme: "SchemeName")
+	    uploadToTestflight(username: appleID)
+	    // You can also use other beta testing services here (run `fastlane actions`)
+	}
+	*/
+	
+	/*
+	func deployLane(withOptions options:[String: String]?) {
+        // ...
+        if let submit = options?["submit"], submit == "true" {
+            // Only when submit is true
+        }
+        // ...
+        incrementBuildNumber(buildNumber: options?["build_number"])
+        // ...
+    }
+	*/
+	
+	
+	
 }
